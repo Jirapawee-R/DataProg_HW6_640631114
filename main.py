a2ea37130372e970598ae54c8954a2d1f91fbac6
Jirapawee-R <42169410+Jirapawee-R@users.noreply.github.com>
2021-09-08 03:00:25 +0700

    Calculate Average total income of each region

--- main.py
+++ main.py
@@ -11,3 +11,10 @@ df_sold_by_region = df.groupby(['region'], sort=False)[['4046', '4225', '4770', 
 # %%
 # Average sales price grouped by region
 df_price_by_region = df.groupby(['region'], sort=False)[['AveragePrice']].mean()
+
+# %%
+# Create DataFrame contain average sales price and total volume grouped by region
+df_income_by_region = df.groupby(['region'], sort=False).agg({'AveragePrice': 'mean', 'Total Volume': 'sum'})
+
+# Create new column named "Total Income" whose value came from multiplying price with volume to the dataframe
+df_income_by_region['TotalIncome'] = df_income_by_region['AveragePrice'] * df_income_by_region['Total Volume']
