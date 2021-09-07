a96808076ef14b63e431710ddd6e7c0c51350f02
Jirapawee-R <42169410+Jirapawee-R@users.noreply.github.com>
2021-09-08 02:42:49 +0700

    Calculate average sales price of each region

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ print(df.dtypes)
 
 # %%
 # Create DataFrame of sales amount and detail grouped by region
-df_by_region = df.groupby(['region'], sort=False)[['4046', '4225', '4770', 'Total Volume']].sum()
+df_sold_by_region = df.groupby(['region'], sort=False)[['4046', '4225', '4770', 'Total Volume']].sum()
 
+# %%
+# Average sales price grouped by region
+df_price_by_region = df.groupby(['region'], sort=False)[['AveragePrice']].mean()
