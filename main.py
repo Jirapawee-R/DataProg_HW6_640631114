e4f38f2680a29071c73851b3f8b506352155118f
Jirapawee-R <42169410+Jirapawee-R@users.noreply.github.com>
2021-09-08 02:37:16 +0700

    Find total amount of avocado sold in each region

--- main.py
+++ main.py
@@ -2,4 +2,9 @@
 import pandas as pd
 
 df = pd.read_csv("~/Downloads/avocado.csv")
+print(df.dtypes)
+
+# %%
+# Create DataFrame of sales amount and detail grouped by region
+df_by_region = df.groupby(['region'], sort=False)[['4046', '4225', '4770', 'Total Volume']].sum()
 
